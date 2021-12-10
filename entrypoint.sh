dc1b336e23ea265ad86916f1a7e61562225e173d
Jackie Rauhut <33187974+rauhut@users.noreply.github.com>
2021-12-10 15:38:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # somehow get the api docs from their repo
 # send it to opslevel (should have a curl request for this?)
 
-api_url=""
-echo $api_url
+#api_url=""
+echo "testing"
 
-api_docs=$(curl "${api_url}" | jq ".name")
-echo $api_docs
+#api_docs=$(curl "${api_url}" | jq ".name")
+#echo $api_docs
 
-echo "::set-output name=api_docs::$pokemon_name"
+# echo "::set-output name=api_docs::$pokemon_name"
